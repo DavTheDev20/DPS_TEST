e383f4c0956d233fbcdc8386d3b28ded2b09493a
DavTheDev20 <reiddavin@outlook.com>
2022-10-25 21:02:26 -0400

    Make UI changes and implement action to edit deal button.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
-// import data from './fakedata.json';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import { Form, InputGroup, Spinner } from 'react-bootstrap';
+import { Form, InputGroup, Spinner, Navbar, Container } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [show, setShow] = useState(false);
+  const [showAddDealMenu, setShowAddDealMenu] = useState(false);
+  const [showEditDealMenu, setShowEditDealMenu] = useState(false);
+  const [dealInFocus, setDealInFocus] = useState({
+    name: '',
+    relationshipManager: '',
+    dealAmount: '',
+  });
   const [newDealData, setNewDealData] = useState({
     name: '',
     relationshipManager: '',
@@ -29,7 +34,7 @@ function App() {
     getDealData();
   }, []);
 
-  const handleChange = ({ target }) => {
+  const handleAddDealChange = ({ target }) => {
     const { name, value } = target;
 
     if (name === 'name') {
@@ -59,6 +64,8 @@ function App() {
     }
   };
 
+  const handleEditDealChange = ({ target }) => {};
+
   const handleAddNewDeal = (e) => {
     e.preventDefault();
     axios({
@@ -73,7 +80,7 @@ function App() {
           relationshipManager: '',
           dealAmount: '',
         });
-        setShow(false);
+        setShowAddDealMenu(false);
         getDealData();
         return;
       })
@@ -83,31 +90,39 @@ function App() {
       });
   };
 
-  const handleShow = () => setShow(true);
-  const handleClose = () => setShow(false);
+  const handleShowAddDealMenu = () => setShowAddDealMenu(true);
+  const handleCloseAddDealMenu = () => setShowAddDealMenu(false);
+  const handleShowEditDealMenu = () => setShowEditDealMenu(true);
+  const handleCloseEditDealMenu = () => setShowEditDealMenu(false);
 
   return (
     <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#">
+            <img
+              src="https://img.icons8.com/clouds/100/000000/technology.png"
+              alt="DaxTech Logo"
+              width="60"
+            />{' '}
+            DaxTech
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
       <h1 style={{ textAlign: 'center', margin: '2% 0' }}>
         Welcome to DPS (Deal Processing System)
       </h1>
       <div>
-        <Button
-          style={{ position: 'absolute', right: '6%', top: '5%' }}
-          onClick={handleShow}
-        >
-          Create New Deal
-        </Button>
-        <Modal show={show} onHide={handleClose}>
+        <Modal show={showAddDealMenu} onHide={handleCloseAddDealMenu}>
           <Modal.Header closeButton>
-            <Modal.Title>Creat New Deal</Modal.Title>
+            <Modal.Title>Create New Deal</Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <InputGroup className="mb-3">
               <InputGroup.Text>Name</InputGroup.Text>
               <Form.Control
                 name="name"
-                onChange={handleChange}
+                onChange={handleAddDealChange}
                 value={newDealData.name}
               />
             </InputGroup>
@@ -115,7 +130,7 @@ function App() {
             <Form.Select
               className="mb-3"
               name="relationshipManager"
-              onChange={handleChange}
+              onChange={handleAddDealChange}
               value={newDealData.relationshipManager}
             >
               <option>Select a Relationship Manager for your deal</option>
@@ -125,7 +140,7 @@ function App() {
             <InputGroup>
               <Form.Control
                 name="dealAmount"
-                onChange={handleChange}
+                onChange={handleAddDealChange}
                 value={newDealData.dealAmount}
               />
               <InputGroup.Text>Deal Amount</InputGroup.Text>
@@ -138,12 +153,15 @@ function App() {
           </Modal.Footer>
         </Modal>
         <Table striped bordered hover style={{ textAlign: 'center' }}>
-          <thead>
+          <thead className="mb-5">
             <tr>
               <th>Deal ID</th>
               <th>Name</th>
               <th>Relationship Manager</th>
               <th>Deal Amount</th>
+              <th>
+                <Button onClick={handleShowAddDealMenu}>Create New Deal</Button>
+              </th>
             </tr>
           </thead>
           <tbody>
@@ -163,15 +181,61 @@ function App() {
             </tr>
             {dealData.map((deal) => {
               return (
-                <tr>
-                  <td>{deal.id}</td>
-                  <td>{deal.name}</td>
-                  <td>{deal.relationshipManager}</td>
-                  <td>{deal.dealAmount}</td>
-                  <td>
-                    <Button variant="danger">Edit Deal</Button>
-                  </td>
-                </tr>
+                <>
+                  <tr key={deal.id}>
+                    <td>{deal.id}</td>
+                    <td>{deal.name}</td>
+                    <td>{deal.relationshipManager}</td>
+                    <td>$ {deal.dealAmount.toLocaleString()}</td>
+                    <td>
+                      <Button
+                        variant="danger"
+                        onClick={() => {
+                          handleShowEditDealMenu();
+                          setDealInFocus(deal);
+                        }}
+                      >
+                        Edit Deal
+                      </Button>
+                    </td>
+                  </tr>
+                  <Modal
+                    show={showEditDealMenu}
+                    onHide={handleCloseEditDealMenu}
+                  >
+                    <Modal.Header closeButton>
+                      Edit Deal ({dealInFocus.name})
+                    </Modal.Header>
+                    <Modal.Body>
+                      <InputGroup className="mb-3">
+                        <InputGroup.Text>Name</InputGroup.Text>
+                        <Form.Control
+                          value={dealInFocus.name}
+                          name="focusedName"
+                        />
+                      </InputGroup>
+                      <Form.Select
+                        value={dealInFocus.relationshipManager}
+                        disabled
+                        className="mb-3"
+                      >
+                        <option value={dealInFocus.relationshipManager}>
+                          {dealInFocus.relationshipManager}
+                        </option>
+                      </Form.Select>
+                      <InputGroup>
+                        <Form.Control
+                          value={dealInFocus.dealAmount}
+                          name="focusedDealAmount"
+                        />
+                        <InputGroup.Text>Deal Amount</InputGroup.Text>
+                      </InputGroup>
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button variant="warning">Submit Change</Button>
+                    </Modal.Footer>
+                  </Modal>
+                </>
               );
             })}
           </tbody>
